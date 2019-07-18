c694319581f70041e46fdb299a0c02ed53361425
Iain Walker <iain.walker@avenir-technology.com>
2019-07-18 08:47:11 +0100

    Commit as laptop going bye-bye

--- ruby/lib/jobcollection.rb
+++ ruby/lib/jobcollection.rb
@@ -26,8 +26,27 @@ class JobCollection
   end
 
   def recurse_job_dependencies job, depth
-    if depth >= @jobs.length
+    if depth > @jobs.length
       raise JobCircularDependencyError, 'circular dependency chain detected'
     end
+
+    dependency = job.get_dependency
+    unless dependency.nil?
+      dependency_populated = find_job(dependency.key)
+      if dependency_populated.nil?
+        raise JobError, 'Dependency does not exist'
+      end
+
+      unless dependency_populated.get_dependency.nil?
+        puts "#{dependency_populated.get_dependency.key}"
+        job.set_dependency(dependency_populated)
+
+        recurse_job_dependencies(job, depth + 1)
+      end
+    end
+  end
+
+  def find_job key
+    @jobs.find { |job| job.key == key }
   end
 end
